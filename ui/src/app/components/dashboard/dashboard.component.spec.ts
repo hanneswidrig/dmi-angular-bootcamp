c9254794c7ce65001f2c351776b7699bf45583b9
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2022-01-31 15:42:41 -0500

    fix unit tests

--- ui/src/app/components/dashboard/dashboard.component.spec.ts
+++ ui/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,18 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { ApiService } from 'src/app/services/api.service';
 import { DashboardComponent } from './dashboard.component';
+import { WeatherSummary } from '../../services/api.service.types';
 
 describe('DashboardComponent', () => {
     let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
 
+    let apiService: ApiService;
+
+    const mockWeatherSummary: WeatherSummary = {
+        guid: 'd74b4f8c-48a3-4a4b-9b89-a3a560bc309a',
+        city: 'Indianapolis',
+        state: 'IN',
+        description: 'sunny',
+        currentHumidity: 80,
+        currentWindSpeed: 10,
+        currentTemperature: 75,
+    };
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [DashboardComponent],
+            imports: [RouterTestingModule, HttpClientModule],
         }).compileComponents();
+
+        apiService = TestBed.inject(ApiService);
     });
 
     beforeEach(() => {
+        spyOn(apiService, 'getWeatherSummary').and.returnValue(of([mockWeatherSummary]));
+
         fixture = TestBed.createComponent(DashboardComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -20,5 +41,6 @@ describe('DashboardComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+        expect(component.weatherSummaries).toEqual([mockWeatherSummary]);
     });
 });
